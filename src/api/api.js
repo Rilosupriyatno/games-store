3082e4ff1280a0fa70f1f495afafa3b1ad0472b9
Rilosupriyatno <rilobahtiar21@gmail.com>
2023-05-13 11:46:57 +0700

    update package,api

---
+++ src/api/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const options = axios.create({
+  baseURL: "https://free-to-play-games-database.p.rapidapi.com/api",
+  method: "GET",
+  url: "https://free-to-play-games-database.p.rapidapi.com/api/filter",
+  params: {
+    tag: "3d.mmorpg.fantasy.pvp",
+    platform: "pc",
+  },
+  headers: {
+    "X-RapidAPI-Key": "ae30c0d2b7mshe6703b979142769p164ffcjsn3d5dcbf9f15d",
+    "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+  },
+});
+export default options;
