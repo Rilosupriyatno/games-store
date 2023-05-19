6689991246e47ea8b90a08a11cc5fc9ce4013239
Rilosupriyatno <rilobahtiar21@gmail.com>
2023-05-20 01:50:50 +0700

    error

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -13,7 +13,19 @@ const Detail = () => {
     async function fetchData() {
       // You can await here
       try {
-        const response = await axios.get(`${options(url)}/?id=${id}`);
+        const response = await axios.get(
+          "https://free-to-play-games-database.p.rapidapi.com/api/filter",
+          {
+            params: {
+              id: id,
+            },
+            headers: {
+              "X-RapidAPI-Key":
+                "ae30c0d2b7mshe6703b979142769p164ffcjsn3d5dcbf9f15d",
+              "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+            },
+          }
+        );
         setItems(response.data);
       } catch (error) {
         console.error(error);
@@ -21,24 +33,20 @@ const Detail = () => {
     }
     fetchData();
   }, []);
+  if (items === null) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <section id="list-games" className="mt-4">
       <h2>List Games</h2>
 
       <div className="grid gap-10">
-        {items.map(({ id, thumbnail, title }) => {
-          return (
-            <article
-              key={id}
-              className="p-5 rounded-lg h-full background: var(--color-bg-variant);"
-            >
-              <div className="rounded-lg overflow-hidden">
-                <img src={thumbnail} alt={title} />
-              </div>
-              <h3>{title}</h3>
-            </article>
-          );
-        })}
+        <article className="p-5 rounded-lg h-full background: var(--color-bg-variant);">
+          <div className="rounded-lg overflow-hidden">
+            <img src={items.thumbnail} alt={items.title} />
+          </div>
+          <h3>{items.title}</h3>
+        </article>
       </div>
     </section>
   );
